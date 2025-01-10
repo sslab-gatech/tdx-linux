a8d3af53166f9852e6c16d36aee41fdbdefe6664
Jaewon Hur <hurjaewon@snu.ac.kr>
2025-01-10 19:56:50 +0000

    Fix vm_succeed
    Signed-off-by: Jaewon Hur <hurjaewon@snu.ac.kr>

--- arch/x86/kvm/vmx/seam.c
+++ arch/x86/kvm/vmx/seam.c
@@ -598,8 +598,8 @@ static int check_entry_info(struct kvm_vcpu *vcpu, u8 *vmcs)
 static int vmx_succeed(struct kvm_vcpu *vcpu)
 {
 	vmx_set_rflags(vcpu, vmx_get_rflags(vcpu)
-			& ~(X86_EFLAGS_PF | X86_EFLAGS_AF | X86_EFLAGS_ZF |
-			    X86_EFLAGS_SF | X86_EFLAGS_OF));
+			& ~(X86_EFLAGS_CF | X86_EFLAGS_PF | X86_EFLAGS_AF |
+                X86_EFLAGS_ZF | X86_EFLAGS_SF | X86_EFLAGS_OF));
 	return kvm_skip_emulated_instruction(vcpu);
 }
 
